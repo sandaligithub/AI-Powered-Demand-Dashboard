e834fed39a95fb37167025eaafae348f85e36cf7
sandaligithub <160390800+sandaligithub@users.noreply.github.com>
2026-04-10 12:07:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1014,4 +1014,5 @@ def process_upload(n_clicks, sales_content, products_content, stores_content):
 server = app.server
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8050)
+    port = int(os.environ.get("PORT", 8050))
+    app.run(host="0.0.0.0", port=port)
